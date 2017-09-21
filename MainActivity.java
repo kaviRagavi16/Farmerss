0305fc5a3f7d8bcd9abd9ba869449daf8858efd7
kaviRagavi16 <kavibscct@gmail.com>
2017-09-21 16:18:18 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,17 +1,59 @@
-package com.example.farmers;
+package com.example.listview;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity
+{
+	ListView listview;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		listview = (ListView) findViewById(R.id.listView1);
+		String[] values = new String[] { "Android List View",
+				"Adapter implementation", "Simple List View In Android",
+				"Create List View Android", "Android Example",
+				"List View Source Code", "List View Array Adapter",
+				"Android Example List View" };
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, android.R.id.text1, values);
+		listview.setAdapter(adapter);
+		listview.setOnItemClickListener(new OnItemClickListener() 
+		{
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id)
+			{
+
+				// ListView Clicked item index
+				int itemPosition = position;
+
+				// ListView Clicked item value
+				String itemValue = (String) listview
+						.getItemAtPosition(position);
+
+				// Show Alert
+				Toast.makeText(
+						getApplicationContext(),
+						"Position :" + itemPosition + "  ListItem : "
+								+ itemValue, Toast.LENGTH_LONG).show();
+
+			}
+
+		});
 	}
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
