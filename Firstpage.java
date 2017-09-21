060a4912c4af18963cf47d7d7e8d20e3856f1545
kaviRagavi16 <kavibscct@gmail.com>
2017-09-21 16:26:35 +0530

    Add files via upload

--- Firstpage.java
+++ Firstpage.java
@@ -1,22 +1,39 @@
 package com.example.farmers;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends ActionBarActivity {
+
+public class Firstpage extends ActionBarActivity {
+	Button b1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_firstpage);
+		b1 = (Button) findViewById(R.id.button2);
+		b1.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				Intent next = new Intent(Firstpage.this, MainActivity.class);
+				startActivity(next);
+				// TODO Auto-generated method stub
+
+			}
+		});
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.firstpage, menu);
 		return true;
 	}
 
